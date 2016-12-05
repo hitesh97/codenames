fc553861d6ddf1148791725f9c48589f75d72daa
Luke Chinworth <lukechinworth@gmail.com>
2016-12-05 10:40:54 -0800

    flesh out rollup config for minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,39 @@
-import svelte from 'rollup-plugin-svelte';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import nodeGlobals from 'rollup-plugin-node-globals';
 import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import nodeGlobals from 'rollup-plugin-node-globals';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import svelte from 'rollup-plugin-svelte';
+import buble from 'rollup-plugin-buble';
+import uglify from 'rollup-plugin-uglify';
 
+
+let plugins = [
+    commonjs({
+        namedExports: {
+            'knuth-shuffle': [ 'knuthShuffle' ]
+        }
+    }),
+    replace({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    nodeGlobals(),
+    nodeResolve({
+        jsnext: true
+    }),
+    svelte(),
+    buble()
+];
+
+let isProduction = process.env.NODE_ENV === 'production';
+
+if (isProduction) {
+    plugins.push(uglify());
+}
 
 export default {
     entry: 'src/index.js',
     dest: 'dist/bundle.js',
     format: 'iife',
-    plugins: [
-        commonjs({
-            namedExports: {
-                'knuth-shuffle': [ 'knuthShuffle' ]
-            }
-        }),
-        nodeGlobals(),
-        nodeResolve({
-            jsnext: true
-        }),
-        svelte()
-    ],
-    sourceMap: true
+    plugins,
+    sourceMap: !isProduction
 }
